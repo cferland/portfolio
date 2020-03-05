53304fd7845ee5e96d3c7196d0e6fc643b86d072
Corey Ferland <ferlandc@gmail.com>
2020-03-05 09:25:05 -0500

    added photo, layout changes

--- src/components/home.js
+++ src/components/home.js
@@ -3,41 +3,48 @@ import React from 'react';
 function Home() {
   return (
     <main>
-      <p>
-        Welcome to my portfolio!
-      </p>
-      <p>
-        My name is Corey Ferland and I am a Full Stack Web Developer with a background in graphic design,
-        digital marketing, content management, and client services.
-        While my career and studies have taken me into a variety of fields,
-        I began learning the basics of web development in high school and have continued to build on that knowledge over the past 10 years.
-      </p>
-      <p>
-        I strive to incorporate efficiency and best practices in all the applications that I develop,
-        while maintaining a user experience that is straightforward and intuitive.
-        I am familiar with Agile methodologies and have experience working as part of a team as both a designer and a developer,
-        providing me with a unique perspective that allows me to seamlessly integrate design concepts into the
-        applications I create.
-      </p>
-      <p>
-        My current skills include such technologies as:
-      </p>
-      <ul>
-        <li>• <span className="no-gradient">HTML</span></li>
-        <li>• <span className="no-gradient">CSS</span></li>
-        <li>• <span className="no-gradient">JavaScript</span></li>
-        <li>• <span className="no-gradient">React</span></li>
-        <li>• <span className="no-gradient">Node</span></li>
-        <li>• <span className="no-gradient">Express</span></li>
-        <li>• <span className="no-gradient">SQL</span></li>
-        <li>• <span className="no-gradient">Ruby on Rails</span></li>
-        <li>• <span className="no-gradient">Python</span></li>
-        <li>• <span className="no-gradient">JSON</span></li>
-        <li>• <span className="no-gradient">CLI</span></li>
-        <li>• <span className="no-gradient">Git / GitHub</span></li>
-        <li>• <span className="no-gradient">REST APIs</span></li>
-        <li>• <span className="no-gradient">...and more!</span></li>
-      </ul>
+      <div className="about">
+        <div className="headshot-wrapper">
+          <img className="headshot" src="/images/headshot.png" alt="headshot" />
+        </div>
+        <div className="about-text">
+          <p>
+            Welcome to my portfolio!
+          </p>
+          <p>
+            My name is Corey Ferland and I am a Full Stack Web Developer with a background in graphic design,
+            digital marketing, content management, and client services.
+            While my career and studies have taken me into a variety of fields,
+            I began learning the basics of web development in high school and have continued to build on that knowledge over the past 10 years.
+          </p>
+          <p>
+            I strive to incorporate efficiency and best practices in all the applications that I develop,
+            while maintaining a user experience that is straightforward and intuitive.
+            I am familiar with Agile methodologies and have experience working as part of a team as both a designer and a developer,
+            providing me with a unique perspective that allows me to seamlessly integrate design concepts into the
+            applications I create.
+          </p>
+          <p>
+            My current skills include such technologies as:
+          </p>
+          <ul>
+            <li>• <span className="no-gradient">HTML</span></li>
+            <li>• <span className="no-gradient">CSS</span></li>
+            <li>• <span className="no-gradient">JavaScript</span></li>
+            <li>• <span className="no-gradient">React</span></li>
+            <li>• <span className="no-gradient">Node</span></li>
+            <li>• <span className="no-gradient">Express</span></li>
+            <li>• <span className="no-gradient">SQL</span></li>
+            <li>• <span className="no-gradient">Ruby on Rails</span></li>
+            <li>• <span className="no-gradient">Python</span></li>
+            <li>• <span className="no-gradient">JSON</span></li>
+            <li>• <span className="no-gradient">CLI</span></li>
+            <li>• <span className="no-gradient">Git / GitHub</span></li>
+            <li>• <span className="no-gradient">REST APIs</span></li>
+            <li>• <span className="no-gradient">...and more!</span></li>
+          </ul>
+        </div>
+      </div>
     </main>
   )
 }
--- src/components/projects.js
+++ src/components/projects.js
@@ -3,39 +3,44 @@ import React from 'react';
 function Projects() {
   return (
     <main>
-      <a href="http://tarot-universe.surge.sh/" target="_blank" rel="noopener noreferrer">
+      <div className="projects">
+      <a href="https://github.com/cferland/tarot-app" target="_blank" rel="noopener noreferrer">
         <h2>Tarot Card App</h2>
-        <img src="/images/tarot-universe.png" alt="Tarot Card App" />
       </a>
       <p>
         Tarot Universe is a tarot card reading application I developed independently using HTML, CSS, and JavaScript,
-        as well as a publicly available API for each of the card descriptions. 
-        The project utilizes CSS animations, DOM manipulation through JavaScript, and API calls using axios.
+          as well as a publicly available API for each of the card descriptions. 
+          The project utilizes CSS animations, DOM manipulation through JavaScript, and API calls using axios.
+          
       </p>
-      <a href="http://dnd-char-creator.surge.sh/" target="_blank" rel="noopener noreferrer">
-        <h2>Dungeons & Dragons Character Creator</h2>
-        <img src="/images/dnd-char.png" alt="DnD Character Creator" />
-      </a>
+      <a href="http://tarot-universe.surge.sh/" target="_blank" rel="noopener noreferrer">
+        <img src="/images/tarot-universe.png" alt="Tarot Card App" />
+        </a>
+        <a href="https://github.com/cferland/dnd-char-creator" target="_blank" rel="noopener noreferrer">
+          <h2>Dungeons & Dragons Character Creator</h2>
+        </a>
       <p>
-        This is the first full React app I developed on my own, offering a quick and easy way for users
+        This is a React app I developed on my own, offering a quick and easy way for users
         to generate a character for the popular Dungeons and Dragons tabletop roleplaying game.
         The application relies on the <a href="http://www.dnd5eapi.co/" target="_blank" rel="noopener noreferrer">D&D 5e API</a> and makes use of
         conditional rendering, HTML forms, and stateful components all based in React.
       </p>
-      <a href="http://pixel-pals.s3-website-us-east-1.amazonaws.com/" target="_blank" rel="noopener noreferrer">
-        <h2>Pixel Pals Website</h2>
-        <img src="/images/pixel-pals.png" alt="Pixel Pals Website" />
-      </a>
+      <a href="http://dnd-char-creator.surge.sh/" target="_blank" rel="noopener noreferrer">
+        <img src="/images/dnd-char.png" alt="DnD Character Creator" />
+        </a>
+        <a href="https://github.com/cferland/pixel-pal" target="_blank" rel="noopener noreferrer">
+          <h2>Pixel Pals Website</h2>
+        </a>
       <p>
         Pixel Pals is a small passion project of mine which features its own backend database.
         The website allows users to create an account, customize their own avatar, as well as view and comment
         on other users' profiles. All of the art featured on the site was created by me, and user data is saved
         to a backend using Ruby on Rails and a custom API. 
       </p>
-      <a href="http://waverlyinn.surge.sh" target="_blank" rel="noopener noreferrer">
-        <h2>Waverly Inn Website Redesign</h2>
-        <img src="/images/waverly-inn.png" alt="Waverly Inn Restaurant" />
-      </a>
+      <a href="http://pixel-pals.s3-website-us-east-1.amazonaws.com/" target="_blank" rel="noopener noreferrer">
+        <img src="/images/pixel-pals.png" alt="Pixel Pals Website" />
+        </a>
+          <h2>Waverly Inn Website Redesign</h2>
       <p>
         The Waverly Inn is a popular yet exclusive restaurant in New York City.
         Their website was redesigned by a small team of UX designers as a learning opportunity,
@@ -43,6 +48,10 @@ function Projects() {
         This project relied on Agile methodologies, user stories, wireframes and mockups, 
         and was an excellent opportunity to work collaboritively with both designers and developers. 
       </p>
+      <a href="http://waverlyinn.surge.sh" target="_blank" rel="noopener noreferrer">
+        <img src="/images/waverly-inn.png" alt="Waverly Inn Restaurant" />
+      </a>
+      </div>
     </main>
   )
 }
